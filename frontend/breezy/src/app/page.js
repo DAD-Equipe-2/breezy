d11eda29216bf492a20e169739fddfb641b92127
Robin <robin.devalle@gmail.com>
2025-06-15 17:58:04 +0200

    Update Post.Js and Creation Textbox.js

--- frontend/breezy/src/app/page.js
+++ frontend/breezy/src/app/page.js
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import Post from "../components/post";
+import TextBox from "../components/textbox";
 
 export default function Home() {
   return (
-   <Post></Post>
+   <TextBox></TextBox>
   );
 }
