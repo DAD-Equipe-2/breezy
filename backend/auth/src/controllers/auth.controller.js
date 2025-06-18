9c57bf23db6eb466f618126d1576275569c23fe5
Paulin <paulin.renault14@gmail.com>
2025-06-18 20:08:05 +0200

    update auth routes with a token middleware

--- backend/auth/src/controllers/auth.controller.js
+++ backend/auth/src/controllers/auth.controller.js
@@ -2,11 +2,10 @@ const jwt    = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 
-const User = require("../models/user.model");
+const User = require('../models/user.model');
 
 
 exports.register = async (req, res) => {
-    // Add middleware to check if the request body contains username and password
     const { username, password } = req.body;
 
     try {
@@ -18,15 +17,8 @@ exports.register = async (req, res) => {
             });
         }
 
-        // Check if the password is strong enough
-        if (password.length < 6) {
-            return res.status(400).json({
-                message: 'Password must be at least 6 characters long'
-            });
-        }
-
         // Check if the username is valid
-        const usernameRegex = /^[a-zA-Z0-9_]{3,}$/; // At least 3 characters, alphanumeric and underscores
+        const usernameRegex = /^[a-zA-Z0-9_]{3,}$/;     // At least 3 characters, alphanumeric and underscores
         if (!usernameRegex.test(username)) {
             return res.status(400).json({
                 message: 'Username must be at least 3 characters long and can only contain letters, numbers, and underscores'
@@ -34,7 +26,7 @@ exports.register = async (req, res) => {
         }
 
         // Check if the password is valid
-        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/; // At least 6 characters, one uppercase, one lowercase, one number
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/;     // At least 6 characters, one uppercase, one lowercase, one number
         if (!passwordRegex.test(password)) {
             return res.status(400).json({
                 message: 'Password must be at least 6 characters long and contain at least one uppercase letter, one lowercase letter, and one number'
@@ -44,57 +36,38 @@ exports.register = async (req, res) => {
         // Create a new user
         const newUser = new User({
             username: username,
-            password: bcrypt.hashSync(password, 10) // Hash the password
+            password: password
         });
-
         await newUser.save();
 
-        return res.status(201).json({
-            message: 'User registered successfully',
-            user: {
-                username: newUser.username
-            }
-        });
-    } 
+        return res.status(201).json({ message: 'User registered successfully' });
+    }
     catch (error) {
-        return res.status(500).json({
-            message: 'Internal server error'
-        });
+        return res.status(500).json({ message: 'Internal server error' });
     }
 };
-
 
 
 exports.login = async (req, res) => {
     const { username, password } = req.body;
 
     try {
-
-        // Check if the user exists
-        const user = User.findOne({ username: username });
-        if (!user) {
-            return res.status(404).json({
-                message: 'User not found'
-            });
-        }
-
-        // Check if the password is correct
-        const isPasswordValid = bcrypt.compareSync(password, user.password);
-        if (!isPasswordValid) {
-            return res.status(401).json({
-                message: 'Invalid password'
-            });
+        // Check if the user exists and if the password is correct
+        const user = await User.findOne({ username: username });
+        if (!user || !(bcrypt.compareSync(password, user.password))) {
+            return res.status(401).json({ message: 'Invalid credentials' });
         }
 
         // Create a JWT token payload
         const payload = {
             username: user.username,
+            role: user.role
         };
         
         // Sign the JWT token with the secret key and set an expiration time
-        const accessToken = jwt.sign(payload, process.env.ACCESS_JWT_KEY, { expiresIn: '24h' });
-       
-        const expiresIn = Math.floor(Date.now() / 1000) + (24 * 60 * 60); // 24 hours 
+        const accessToken = jwt.sign(payload, process.env.ACCESS_JWT_KEY, { expiresIn: '1h' });
+
+        const expiresIn = Math.floor(Date.now() / 1000) + (1 * 60 * 60); // 1 hour
 
         // Set the token in a cookie with httpOnly and secure flags
         res.cookie('accessToken', accessToken, {
@@ -107,62 +80,15 @@ exports.login = async (req, res) => {
             message: 'Login successful',
             accessToken_expiresIn: expiresIn,
         });
-    
-    } 
+    }
     catch (error) {
-        return res.status(500).json({
-            message: 'Internal server error'
-        });
+        return res.status(500).json({ message: 'Internal server error' });
     }
 };
 
 
-
 exports.verify = async (req, res) => {
-
-    // Get the token from the Authorization header
-    const token = req.headers['authorization']
-    
-    // Check if the token is provided
-    if (!token) {
-        return res.status(401).json({
-            message: 'No token provided'
-        });
-    }
-
-    // Check if the token starts with 'Bearer '
-    if (!token.startsWith('Bearer ')) {
-        return res.status(401).json({
-            message: 'Invalid token format'
-        });
-    }
-
-    // Extract the token from the Authorization header
-    const accessToken = token.split(' ')[1];
-
-    // Verify the token
-    jwt.verify(accessToken, process.env.ACCESS_JWT_KEY, (err, decoded) => {
-        if (err) {
-            return res.status(401).json({
-                message: 'Invalid token'
-            });
-        }
-
-        // If the token is valid, check if the user exists in the database
-        User.findOne({ username: decoded.username }, (err, user) => {
-            if (err || !user) {
-                return res.status(404).json({
-                    message: 'User not found'
-                });
-            }
-        });
-
-        // If the user exists, return the user information
-        return res.status(200).json({
-            message: 'Token is valid',
-        });
-        
-    });
+    return res.status(200).json({ message: 'Token is valid' });
 }
 
 
--- backend/auth/src/middlewares/auth.middleware.js
+++ backend/auth/src/middlewares/auth.middleware.js
@@ -1,1 +1,29 @@
-const jwt = require('jsonwebtoken');
+const jwt  = require('jsonwebtoken');
+const User = require('../models/user.model');
+
+
+module.exports = function authenticateJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+
+    // Check if the Authorization header is present
+    if (!authHeader) return res.status(401).json({ message: 'No token provided' });
+
+    // Check if the token starts with 'Bearer '
+    if (!authHeader.startsWith('Bearer ')) return res.status(401).json({ message: 'Invalid token format' });
+
+    // Extract the token from the Authorization header
+    const accessToken = authHeader.split(' ')[1];
+
+    // Verify the token using the secret key
+    jwt.verify(accessToken, process.env.ACCESS_JWT_KEY, (err, decoded) => {
+        if (err) return res.status(401).json({ message: 'Invalid token' });
+   
+        // Check if the user exists in the database
+        User.findOne({ username: decoded.username }, (err, user) => {
+            if (err || !user) return res.status(404).json({ message: 'User not found' });
+        });
+
+        req.user = decoded;
+        next();
+    });
+}
