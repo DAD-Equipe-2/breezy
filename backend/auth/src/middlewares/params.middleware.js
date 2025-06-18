859ee8d310c6a22ec902007008c2da04db32a8af
Paulin <paulin.renault14@gmail.com>
2025-06-18 20:08:20 +0200

    add required body params middleware

---
+++ backend/auth/src/middlewares/params.middleware.js
@@ -0,0 +1,14 @@
+
+module.exports = function requireBodyParams(...requiredParams) {
+    return (req, res, next) => {
+        const missing = requiredParams.filter(param => !(param in req.body));
+
+        if (missing.length > 0) {
+            return res.status(400).json({
+                error: 'Missing required fields',
+                missing
+            });
+        }
+        next();
+    };
+};
