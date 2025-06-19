e8f6366093190ee8fe82ce122aeaa5d6115a952c
Sonixray <loangayrard@gmail.com>
2025-06-19 12:04:30 +0200

    Page paramètre d'un profil

--- frontend/breezy/src/components/input.js
+++ frontend/breezy/src/components/input.js
@@ -1,4 +1,4 @@
-export default function Input({ Hint, Type, FlexType = "flex-col", WithBorder = true, Gap = "gap-2" }) {
+export default function Input({ Hint, Type, FlexType = "flex-col", WithBorder = true, Gap = "gap-2", textarea = false }) {
   return (
     <div
       className={`flex ${FlexType} ${WithBorder ? "border border-foreground" : ""} p-2 w-[90%] ${Gap}`}
@@ -6,11 +6,19 @@ export default function Input({ Hint, Type, FlexType = "flex-col", WithBorder = 
       <p className="text-foreground font-bold font-Roboto text-sm whitespace-nowrap">
         {Hint}
       </p>
-      <input
-        type={Type}
-        defaultValue=""
-        className="text-foreground font-thin text-xs font-Roboto bg-transparent flex-grow focus:outline-none"
-      />
+      {textarea ? (
+        <textarea
+          defaultValue=""
+          rows={6}
+          className="text-foreground font-thin text-xs font-Roboto bg-transparent flex-grow focus:outline-none resize-y p-2"
+        />
+      ) : (
+        <input
+          type={Type}
+          defaultValue=""
+          className="text-foreground font-thin text-xs font-Roboto bg-transparent flex-grow focus:outline-none"
+        />
+      )}
     </div>
   );
 }
