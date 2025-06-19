b895e34f5433b730af94ed30e1af00208c21cc1d
Sonixray <loangayrard@gmail.com>
2025-06-19 10:19:32 +0200

    Profil page improved

--- frontend/breezy/src/app/user/[username]/page.js
+++ frontend/breezy/src/app/user/[username]/page.js
@@ -1,3 +1,4 @@
+import Button from "@/components/button";
 import Post from "@/components/post";
 import BackButton from "@/components/return";
 import { FaCalendar } from "react-icons/fa";
@@ -12,27 +13,36 @@ export default async function UserProfile({ params }) {
         <BackButton/>
       </div>
       <div className="p-4">
-        <div className="flex flex-col">
-          <img
-            src="/profil_picture.jpg"
-            alt="Profile Picture"
-            className="w-24 h-24 rounded-full mb-4"
-          />
-          <p className="text-2xl font-bold">{username}</p>
-          <p className="text-l font-italic">@{username}</p>
-          <br></br>
-          <p className="text-sm text-foreground text-opacity-70">Bio: This is a sample bio for {username}.</p>
-          <div className="flex">
-            <FaCalendar className="text-200 mr-2" />
-            <p className="text-sm text-foreground text-opacity-30">Joined: January 2023</p>
+        <div className="flex items-start justify-between mb-4">
+          <div className="flex flex-col">
+            <img
+              src="/profil_picture.jpg"
+              alt="Profile Picture"
+              className="w-24 h-24 rounded-full mb-4"
+            />
+            <p className="text-2xl font-bold">{username}</p>
+            <p className="text-l font-italic">@{username}</p>
+            <br></br>
+            <p className="text-sm text-foreground text-opacity-70">Bio: This is a sample bio for {username}.</p>
+            <div className="flex">
+              <FaCalendar className="text-200 mr-2" />
+              <p className="text-sm text-foreground text-opacity-30">Joined: January 2023</p>
+            </div>
+            <div className="flex space-x-2 mt-2">
+              <p className="text-sm text-foreground text-opacity-70">217 followings</p>
+              <p className="text-sm text-foreground text-opacity-70">120 followers</p>
+            </div>
           </div>
-          <br/>
-          <hr/>
-          <br/>
-          <Post image={true}></Post>
-          <Post image={false}></Post>
-          <Post image={false}></Post>
-          <Post image={true}></Post>
+          <Button text="Edit profile"></Button>
+        </div>
+        <br/>
+        <hr/>
+        <br/>
+        <div className="flex flex-col">
+          <Post></Post>
+          <Post></Post>
+          <Post></Post>
+          <Post></Post>
         </div>
       </div>
     </div>
