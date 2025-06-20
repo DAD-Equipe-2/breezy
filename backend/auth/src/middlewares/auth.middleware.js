f8c5ba0e0a04fcb4a88260e5651d27a58c4de8ec
Paulin <paulin.renault14@gmail.com>
2025-06-20 11:30:23 +0200

    update token middleware to work with the token renewal as well & fix db error (findOne function doesn't support callbacks anymore)

--- backend/auth/src/middlewares/auth.middleware.js
+++ backend/auth/src/middlewares/auth.middleware.js
@@ -2,28 +2,43 @@ const jwt  = require('jsonwebtoken');
 const User = require('../models/user.model');
 
 
-module.exports = function authenticateJWT(req, res, next) {
-    const authHeader = req.headers.authorization;
+function authenticateJWT(key) {
+    return (req, res, next) => {
+        const authHeader = req.headers.authorization;
 
-    // Check if the Authorization header is present
-    if (!authHeader) return res.status(401).json({ message: 'No token provided' });
+        try {
+            // Check if the Authorization header is present
+            if (!authHeader) return res.status(401).json({ message: 'No token provided' });
 
-    // Check if the token starts with 'Bearer '
-    if (!authHeader.startsWith('Bearer ')) return res.status(401).json({ message: 'Invalid token format' });
+            // Check if the token starts with 'Bearer '
+            if (!authHeader.startsWith('Bearer ')) return res.status(401).json({ message: 'Invalid token format' });
 
-    // Extract the token from the Authorization header
-    const accessToken = authHeader.split(' ')[1];
+            // Extract the token from the Authorization header
+            const token = authHeader.split(' ')[1];
 
-    // Verify the token using the secret key
-    jwt.verify(accessToken, process.env.ACCESS_JWT_KEY, (err, decoded) => {
-        if (err) return res.status(401).json({ message: 'Invalid token' });
-   
-        // Check if the user exists in the database
-        User.findOne({ username: decoded.username }, (err, user) => {
-            if (err || !user) return res.status(404).json({ message: 'User not found' });
-        });
+            // Verify the token using the secret key
+            jwt.verify(token, key, async (err, decoded) => {
+                if (err) {
+                    if (err.name === 'TokenExpiredError') {
+                        return res.status(403).send({ message: 'Token expired' });
+                    } else {
+                        return res.status(401).send({ message: 'Invalid token' });
+                    }
+                }
 
-        req.user = decoded;
-        next();
-    });
+                // Check if the user exists in the database
+                const user = await User.findOne({ username: decoded.username });
+                if (!user) return res.status(404).json({ message: 'User not found' });
+
+                req.user = decoded;
+                req.token = token;
+                next();
+            });
+        }
+        catch (err) {
+            return res.status(500).json({ message: 'Internal server error' });
+        }
+    }
 }
+
+module.exports = { authenticateJWT };
