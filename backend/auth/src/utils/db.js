1c95d3125eac795bd7fbd39d5d3e303d94ea983c
Paulin <paulin.renault14@gmail.com>
2025-06-20 11:31:32 +0200

    update db connection script

--- backend/auth/src/utils/db.js
+++ backend/auth/src/utils/db.js
@@ -1,14 +1,11 @@
 const mongoose = require('mongoose');
 
 
-module.exports = async function authenticateJWT() {
-    
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.info('✅ MongoDB connected');
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('✅ Connected to MongoDB'))
+.catch(err => {
+    console.error('❌ MongoDB connection error:', err);
+    process.exit(1);
+});
 
-    } catch (err) {
-        console.error('❌ MongoDB connection error:', err.message);
-        // process.exit(1);
-    }
-};
+module.exports = mongoose;
