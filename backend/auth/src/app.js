a394468dd8b16244d388229ce6569e7d95221bb0
Paulin <paulin.renault14@gmail.com>
2025-06-20 11:28:07 +0200

    move index.js and split with app.js

---
+++ backend/auth/src/app.js
@@ -0,0 +1,22 @@
+const express      = require('express');
+const cors         = require('cors');
+const cookieParser = require('cookie-parser');
+
+const app  = express();
+
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello World !')
+});
+
+require('./routes/auth.route')(app);
+
+
+module.exports = app;
---
+++ backend/auth/src/index.js
@@ -0,0 +1,16 @@
+const { generateJWTKeys } = require('./utils/env');
+generateJWTKeys();
+require('dotenv').config();
+
+const app      = require('./app');
+const mongoose = require('./utils/db');
+
+const port = 3000;
+
+
+app.listen(port, () => {
+    console.info(`Server started on port ${port}`);
+});
+
+
+// Add swagger documentation
