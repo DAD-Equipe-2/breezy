60b0f9ceba509ae5628ada92b0133767eb13e74a
Paulin <paulin.renault14@gmail.com>
2025-06-20 11:31:40 +0200

    update auth routes

--- backend/auth/src/routes/auth.route.js
+++ backend/auth/src/routes/auth.route.js
@@ -1,19 +1,11 @@
-const authController = require('../controllers/auth.controller');
+const authController        = require('../controllers/auth.controller');
 const { requireBodyParams } = require('../middlewares/params.middleware');
-const authenticateJWT = require('../middlewares/auth.middleware');
+const { authenticateJWT }   = require('../middlewares/auth.middleware');
 
 
 module.exports = function(app) {
     app.post('/register', requireBodyParams('username', 'password'), authController.register);
     app.post('/login', requireBodyParams('username', 'password'), authController.login);
-    app.get('/verify', authenticateJWT, authController.verify);
-    app.post('/refresh-token', authenticateJWT, authController.refreshToken);
-    app.delete('/user', authenticateJWT, authController.deleteUser);
+    app.get('/verify', authenticateJWT(process.env.ACCESS_JWT_KEY), authController.verify);
+    app.post('/renew', authenticateJWT(process.env.REFRESH_JWT_KEY), authController.renewToken);
 };
-
-
-// Use /token/refresh for refresh token functionality or /refresh-token
-// Use /user/delete for deleting a user
-
-// Add refresh token route
-// Add a route to delete a user
