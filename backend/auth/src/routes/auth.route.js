e91bb152ee3ebfb539732dffa64e47e1c9c0d3fe
Paulin <paulin.renault14@gmail.com>
2025-06-20 11:37:13 +0200

    change http method for /verify from GET to POST

--- backend/auth/src/routes/auth.route.js
+++ backend/auth/src/routes/auth.route.js
@@ -6,6 +6,6 @@ const { authenticateJWT }   = require('../middlewares/auth.middleware');
 module.exports = function(app) {
     app.post('/register', requireBodyParams('username', 'password'), authController.register);
     app.post('/login', requireBodyParams('username', 'password'), authController.login);
-    app.get('/verify', authenticateJWT(process.env.ACCESS_JWT_KEY), authController.verify);
+    app.post('/verify', authenticateJWT(process.env.ACCESS_JWT_KEY), authController.verify);
     app.post('/renew', authenticateJWT(process.env.REFRESH_JWT_KEY), authController.renewToken);
 };
