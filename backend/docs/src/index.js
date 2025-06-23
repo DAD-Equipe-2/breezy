9f692b233214f810f1697cbcffca7fa541891fe5
Paulin <paulin.renault14@gmail.com>
2025-06-23 20:05:18 +0200

    update swagger conf (documentation service)

--- backend/docs/src/index.js
+++ backend/docs/src/index.js
@@ -10,21 +10,25 @@ const services = [
     {
         name: 'Auth Service',
         url: '/api/v1/auth/docs/swagger.json',
-        description: 'Authentication service for user management, including registration and login.',
-
-        
+    },
+    {
+        name: 'Gateway Service',
+        url: '/api/v1/gateway/docs/swagger.json',
     },
 ];
 
+const swaggerOptions = {
+    explorer: true,
+    swaggerOptions: {
+        urls: services
+    },
+    customSiteTitle: 'Breezy API Docs',
+};
+
 app.use(
     '/',
-    swaggerUi.serve,
-    swaggerUi.setup(null, {
-            swaggerOptions: {
-            urls: services,
-        },
-        customSiteTitle: 'Breezy API Docs',
-    })
+    swaggerUi.serveFiles(null, swaggerOptions),
+    swaggerUi.setup(null, swaggerOptions)
 );
 
 app.listen(PORT, () => {
