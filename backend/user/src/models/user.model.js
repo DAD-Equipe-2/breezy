e0942e2a38f597f34fb98e79acbe3ab2c019a4d0
Paulin <paulin.renault14@gmail.com>
2025-06-23 20:06:54 +0200

    update user model (user service)

---
+++ backend/user/src/models/user.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 3,
+        maxlength: 30,
+    },
+    nickname: {
+        type: String,
+        default: '',
+        trim: true,
+        maxlength: 50,
+    },
+    bio: {
+        type: String,
+        default: ''
+    },
+    avatar: {
+        data: Buffer,
+        contentType: String
+    },
+    followers: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User' 
+    }],
+    following: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User' 
+    }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', UserSchema);
