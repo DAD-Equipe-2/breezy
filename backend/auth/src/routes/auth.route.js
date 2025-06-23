87f536d887bfb563b1c9b0f07fb374577967a1ea
Paulin <paulin.renault14@gmail.com>
2025-06-23 14:43:16 +0200

    use express router instead of function initialization (auth service)

--- backend/auth/src/routes/auth.route.js
+++ backend/auth/src/routes/auth.route.js
@@ -1,7 +1,9 @@
+const express               = require('express');
 const authController        = require('../controllers/auth.controller');
 const { requireBodyParams } = require('../middlewares/params.middleware');
 const { authenticateJWT }   = require('../middlewares/auth.middleware');
 
+const router = express.Router();
 
 /**
  * @swagger
@@ -10,92 +12,91 @@ const { authenticateJWT }   = require('../middlewares/auth.middleware');
  *   description: Authentication related endpoints
 */
 
-module.exports.register = function(app) {
-    /**
-     * @swagger
-     * /register:
-     *   post:
-     *     summary: Register a new user
-     *     tags: [Auth]
-     *     requestBody:
-     *       required: true
-     *       content:
-     *         application/json:
-     *           schema:
-     *             type: object
-     *             properties:
-     *               username:
-     *                 type: string
-     *               password:
-     *                 type: string
-     *     responses:
-     *       201:
-     *         description: User registered successfully
-     *       400:
-     *         description: Bad request
-     */
-    app.post('/register', requireBodyParams('username', 'password'), authController.register);
-}
 
-module.exports.login = function(app) {
-    /**
-     * @swagger
-     * /login:
-     *   post:
-     *     summary: Login a user
-     *     tags: [Auth]
-     *     requestBody:
-     *       required: true
-     *       content:
-     *         application/json:
-     *           schema:
-     *             type: object
-     *             properties:
-     *               username:
-     *                 type: string
-     *               password:
-     *                 type: string
-     *     responses:
-     *       200:
-     *         description: User logged in successfully
-     *       400:
-     *         description: Bad request
-     */
-    app.post('/login', requireBodyParams('username', 'password'), authController.login);
-}
+/**
+ * @swagger
+ * /register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *       400:
+ *         description: Bad request
+ */
+router.post('/register', requireBodyParams('username', 'password'), authController.register);
 
-module.exports.verify = function(app) {
-    /**
-     * @swagger
-     * /verify:
-     *   post:
-     *     summary: Verify access token
-     *     tags: [Auth]
-     *     security:
-     *      - bearerAuth: []
-     *     responses:
-     *       200:
-     *         description: Access token is valid
-     *       401:
-     *         description: Unauthorized
-     */
-    app.post('/verify', authenticateJWT(process.env.ACCESS_JWT_KEY), authController.verify);
-}
 
-module.exports.renew = function(app) {
-    /**
-     * @swagger
-     * /renew:
-     *   post:
-     *     summary: Renew access token using refresh token
-     *     tags: [Auth]
-     *     security:
-     *      - bearerAuth: []
-     *     responses:
-     *       200:
-     *         description: Access token renewed successfully
-     *       401:
-     *         description: Unauthorized
-     */
-    app.post('/renew', authenticateJWT(process.env.REFRESH_JWT_KEY), authController.renewToken);
-}
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: Login a user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User logged in successfully
+ *       400:
+ *         description: Bad request
+ */
+router.post('/login', requireBodyParams('username', 'password'), authController.login);
+
+
+/**
+ * @swagger
+ * /verify:
+ *   post:
+ *     summary: Verify access token
+ *     tags: [Auth]
+ *     security:
+ *      - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Access token is valid
+ *       401:
+ *         description: Unauthorized
+ */
+router.post('/verify', authenticateJWT(process.env.ACCESS_JWT_KEY), authController.verify);
+
+
+/**
+ * @swagger
+ * /renew:
+ *   post:
+ *     summary: Renew access token using refresh token
+ *     tags: [Auth]
+ *     security:
+ *      - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Access token renewed successfully
+ *       401:
+ *         description: Unauthorized
+ */
+router.post('/renew', authenticateJWT(process.env.REFRESH_JWT_KEY), authController.renewToken);
+
+
+module.exports = router;
