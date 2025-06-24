343056be5712bc5d4d7641976f9c30d5acbb0d46
Robin <robin.devalle@gmail.com>
2025-06-24 17:29:36 +0200

    Integration of new post

--- frontend/breezy/src/components/button.js
+++ frontend/breezy/src/components/button.js
@@ -1,10 +1,11 @@
 "use client";
 
-export default function Button({ text, icon, textcolor, textFondSize, paddingX, bordercolor, color, action }) {
+export default function Button({ text, icon, textcolor, textFondSize, paddingX, bordercolor, color, action, disabled = false }) {
   return (
     <div className="flex justify-center items-center">
       <button
         onClick={action}
+        disabled={disabled}
         className={`flex items-center justify-center ${textcolor} border ${bordercolor} ${textFondSize} font-semibold rounded-full shadow-md ${color} max-w-[240px] ${paddingX} py-2 cursor-pointer transition duration-200 ease-in-out hover:opacity-80 hover:scale-105`}
       >
         {icon ? icon : text}
