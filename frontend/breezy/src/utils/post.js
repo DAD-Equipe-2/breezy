e5cc8075561a3c9910727d0c908a468ac193871d
Robin DEVALLE <robin.devalle@gmail.com>
2025-06-25 12:14:31 +0200

    Start Post Integration

--- frontend/breezy/src/utils/post.js
+++ frontend/breezy/src/utils/post.js
@@ -12,3 +12,17 @@ export async function createPost(content) {
     throw error;
   }
 }
+
+// Function to get all posts
+export async function getPosts(username) {
+  try {
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/posts/user/${username}`, {
+      withCredentials: true
+    });
+    console.log("Posts fetched:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+    throw error;
+  }
+}
