9f68af47e6447bcde1bdc05cd95160a868e42b88
Paulin <paulin.renault14@gmail.com>
2025-06-27 05:31:28 +0200

    add little margin bottom for the bio on a user's profile

--- frontend/breezy/src/app/user/[username]/page.js
+++ frontend/breezy/src/app/user/[username]/page.js
@@ -107,7 +107,7 @@ export default function UserProfile({ params }) {
             <p className="text-2xl font-bold">{user.nickname.length > 15 ? `${user.nickname.slice(0, 15)}…` : user.nickname}</p>
             <p className="text-l font-italic">@{user.username}</p>
             <br></br>
-            <p className="text-sm text-foreground text-opacity-70 w-full w-full break-words leading-relaxed ">{user.bio ? user.bio : "No bio provided"}</p>
+            <p className="text-sm text-foreground text-opacity-70 w-full w-full break-words leading-relaxed mb-2">{user.bio ? user.bio : "No bio provided"}</p>
             <div className="flex">
               <FaCalendar className="text-200 mr-2" />
               <p className="text-sm text-foreground text-opacity-30">
